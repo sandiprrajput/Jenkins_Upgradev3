719be349c25d30b6c71923208ad1496011b6c835
Sandip Rajput <60064559+sandiprrajput@users.noreply.github.com>
2024-11-05 21:32:08 +0530

    Update TestGreeter.java
    Added failed test case code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+   @Test
+  public void Intentionally_Failed() {
+    assertTrue(false);
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
